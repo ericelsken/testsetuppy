50467072b309220ee1be03663610889cd5056b33
Eric Elsken <eric.elsken@gmail.com>
2018-03-29 10:54:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from setuptools import setup
 
 setup(
     name='testsetuppy',
-    version='0.1.0',
+    version='0.2.0',
     install_requires=['google-auth','google-api-python-client','google-auth-httplib2'],
+)
+
+setup(
+    name='testsetuppyagain',
+    version='0.2.0',
+    install_requires=['numpy'],
 )
